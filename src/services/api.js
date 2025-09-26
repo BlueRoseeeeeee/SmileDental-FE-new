717908c7c816c6cb982b434b176effc15e9e9a38
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-09-26 17:08:15 +0700

    fix port auth

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base API URL
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'http://localhost:3001/api';
 
 // Create axios instance
 const api = axios.create({
