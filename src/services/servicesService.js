2488d72794b5a01aa641d9acad4097878965c33f
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-10-03 03:04:35 +0700

    config service-Service

---
+++ src/services/servicesService.js
@@ -0,0 +1,12 @@
+/**
+ * @author: HoTram
+ */
+import { serviceApi } from './apiFactory.js';
+
+export const servicesService = {
+  // Lấy danh sách services
+  async getServices(page = 1, limit = 10) {
+    const response = await serviceApi.get(`/service?page=${page}&limit=${limit}`);
+    return response.data;
+  }
+};
