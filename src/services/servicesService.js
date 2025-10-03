fada35a09e58849a912fdbf9888fac04777b4654
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-10-03 14:41:34 +0700

    [servicesService] tạo service mới

--- src/services/servicesService.js
+++ src/services/servicesService.js
@@ -8,5 +8,11 @@ export const servicesService = {
   async getServices(page = 1, limit = 10) {
     const response = await serviceApi.get(`/service?page=${page}&limit=${limit}`);
     return response.data;
+  },
+
+  // Tạo service mới
+  async createService(serviceData) {
+    const response = await serviceApi.post('/service', serviceData);
+    return response.data;
   }
 };
