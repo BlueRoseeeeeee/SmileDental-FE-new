247273f6cd3012e7e77b60f591bd2d0bc9aff76c
Ho Tram <21087811.tram@student.iuh.edu.vn>
2025-10-04 23:33:19 +0700

    export point

--- src/services/index.js
+++ src/services/index.js
@@ -9,7 +9,8 @@ export {
   authApi, 
   roomApi, 
   serviceApi, 
-  userApi 
+  userApi,
+  scheduleApi
 } from './apiFactory.js';
 
 // Services
@@ -17,6 +18,10 @@ export { authService } from './authService.js';
 export { userService } from './userService.js';
 export { default as roomService } from './roomService.js';
 export { servicesService } from './servicesService.js';
+export { default as scheduleService } from './scheduleService.js';
+export { default as slotService } from './slotService.js';
+export { default as scheduleConfigService } from './scheduleConfigService.js';
+export { default as autoScheduleService } from './autoScheduleService.js';
 
 // Utilities
 export { toast } from './toastService.js';
